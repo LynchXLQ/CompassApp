c5c7404a19057146df0f3ab3eba113e47db5e6b9
Linqi Xiao <linqixiao2021@gmail.com>
2022-12-15 10:29:20 -0500

    Add gyroscope for Compass App

--- app/src/main/java/com/example/compassapp/MainActivity.java
+++ app/src/main/java/com/example/compassapp/MainActivity.java
@@ -25,6 +25,9 @@ public class MainActivity extends Activity implements SensorEventListener {
     private TextView tv_orientation_Azimuth_result;
     private TextView tv_orientation_Pitch_result;
     private TextView tv_orientation_Roll_result;
+    private TextView tv_gyroscope_x_result;
+    private TextView tv_gyroscope_y_result;
+    private TextView tv_gyroscope_z_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +41,13 @@ public class MainActivity extends Activity implements SensorEventListener {
         tv_orientation_Azimuth_result = findViewById(R.id.tv_orientation_Azimuth_result);
         tv_orientation_Pitch_result = findViewById(R.id.tv_orientation_Pitch_result);
         tv_orientation_Roll_result = findViewById(R.id.tv_orientation_Roll_result);
+        tv_gyroscope_x_result = findViewById(R.id.tv_gyroscope_x_result);
+        tv_gyroscope_y_result = findViewById(R.id.tv_gyroscope_y_result);
+        tv_gyroscope_z_result = findViewById(R.id.tv_gyroscope_z_result);
 
         // initialize your android device sensor capabilities
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
     }
 
     @Override
@@ -48,8 +55,8 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onResume();
 
         // for the system's orientation sensor registered listeners
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-                SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
@@ -62,34 +69,39 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+            // get the angle around the z-axis rotated
+            float Azimuth_result = Math.round(event.values[0]);
+            float Pitch_result = Math.round(event.values[1]);
+            float Roll_result = Math.round(event.values[2]);
 
-        // get the angle around the z-axis rotated
-        float Azimuth_result = Math.round(event.values[0]);
-        float Pitch_result = Math.round(event.values[1]);
-        float Roll_result = Math.round(event.values[2]);
-
-        tv_orientation_Azimuth_result.setText(Float.toString(Azimuth_result));
-        tv_orientation_Pitch_result.setText(Float.toString(Pitch_result));
-        tv_orientation_Roll_result.setText(Float.toString(Roll_result));
+            tv_orientation_Azimuth_result.setText(Float.toString(Azimuth_result));
+            tv_orientation_Pitch_result.setText(Float.toString(Pitch_result));
+            tv_orientation_Roll_result.setText(Float.toString(Roll_result));
 
 
-        // create a rotation animation (reverse turn degree degrees)
-        RotateAnimation ra = new RotateAnimation(
-                currentDegree,
-                -Azimuth_result,
-                Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f);
+            // create a rotation animation (reverse turn degree degrees)
+            RotateAnimation ra = new RotateAnimation(
+                    currentDegree,
+                    -Azimuth_result,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f);
 
-        // how long the animation will take place
-        ra.setDuration(210);
+            // how long the animation will take place
+            ra.setDuration(210);
 
-        // set the animation after the end of the reservation status
-        ra.setFillAfter(true);
+            // set the animation after the end of the reservation status
+            ra.setFillAfter(true);
 
-        // Start the animation
-        image.startAnimation(ra);
-        currentDegree = -Azimuth_result;
+            // Start the animation
+            image.startAnimation(ra);
+            currentDegree = -Azimuth_result;
+        } else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            tv_gyroscope_x_result.setText(Float.toString(Math.round(event.values[0])));
+            tv_gyroscope_y_result.setText(Float.toString(Math.round(event.values[1])));
+            tv_gyroscope_z_result.setText(Float.toString(Math.round(event.values[2])));
+        }
 
     }
 
